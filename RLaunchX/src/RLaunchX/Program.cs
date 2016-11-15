b29d3107c46978c72dc86b0f5c958de6f5844512
0xFireball <0xFireball@outlook.com>
2016-11-15 12:10:26 -0800

    add  josn

--- RLaunchX/src/RLaunchX/Program.cs
+++ RLaunchX/src/RLaunchX/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RLaunchX
+﻿namespace RLaunchX
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
         }
     }
 }
